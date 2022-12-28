e3cb9067f3954b5d753d7e17578864a6867e8916
Ислам Таштанбеков <islamtastanbekov@MacBook-Air-Islam.local>
2022-12-28 10:44:44 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <header className='header'>
+        <h1 className='title-text'>
+          Photo <p>Gallery</p>
+        </h1>
+      </header>
+      <section className='section'>
+        <img src='https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'></img>
+      </section>
+      <section className='section'>
+        <img src='https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'></img>
+
+      </section>
+      <section className='section'>
+        <img src='https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'></img>
+
+      </section>
+      <section className='section'>
+        <img src='https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'></img>
+
+      </section>
+      <section className='section'>
+        <img src='https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'></img>
+
+      </section>
+      <section className='section'>
+        <img src='https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'></img>
+
+      </section>
+      <footer className='footer'>
+       <h1>
+        Project by <p>Islam Tashtanbekov</p>
+      </h1>
+      </footer>
+    </body>
+  );
+}
+
+export default App;
